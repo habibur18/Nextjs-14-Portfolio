7f0a9c38ef12be8365cfce4c7ff0961e0765eda1
Habibur Rahman <amarhabibur@gmail.com>
2024-12-26 15:48:06 +0600

    ..

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -8,7 +8,7 @@ export const personalData = {
   address: "Dhaka, Bangladesh",
   github: "https://github.com/habibur18",
   facebook: "https://www.facebook.com/RandomHabibur",
-  linkedIn: "https://www.linkedin.com/in/habibur18/",
+  linkedIn: "https://www.linkedin.com/in/habibur19/",
   twitter: "https://twitter.com/amarhabibur",
   stackOverflow: "",
   leetcode: "https://leetcode.com/habibur18/",
